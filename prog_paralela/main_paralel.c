6ea48be8d184e7821aa5dc2446642d9ebff752ea
Arthur Silva <arxdsilva@gmail.com>
2020-05-14 07:53:32 -0300

    update programs

--- prog_paralela/main_paralel.c
+++ prog_paralela/main_paralel.c
@@ -24,95 +24,74 @@ double power(double base, int expoente) {
    return (resultado);
 }
 
-double serie_seno(double x, int termos){
-   MPI_Init(NULL, NULL);
-   int rank;
-   int world;
-   MPI_Comm com = MPI_COMM_WORLD;
-   MPI_Status status;
-   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-   MPI_Comm_size(MPI_COMM_WORLD, &world);
-   double pi = 3.14159265358979323846;
+double serie_seno(double ang, int termos, int h){
    int i;
-   double resultado;
-   resultado = 0;
-   x = (x*pi)/180.0;
+   double resultado = 0.0;
    for(i = 0; i < termos;i++){
-      double r = power(-1, i) * power(x, 2*i + 1) / fatorial(2*i + 1);
-      MPI_Reduce(&r, &resultado, 1, MPI_INT, MPI_SUM, 0, com);
-      // resultado += r;
+      resultado += power(-1, i) * power(ang, 2*i + 1) / fatorial(2*i + 1);
    }
-   printf("\nsen(x), x = ");
+   return (resultado*h);
+}
+
+int main(int argc, char *argv[]){
+   // CONVERTE ANGULO
+   double SENO, COSN, TG;
+   int n, myid, numprocs, i;
+   double pi = 3.14159265358979323846;
+   double ang=0.0;
+   ang = (double) (atof(argv[2]) + (atof(argv[3])/60.0) + (atof(argv[4])/3600.0));
+   ang = (ang*pi)/180.0; // angulo em radianos
+   float tempoI, tempoF;
+   MPI_Init(&argc,&argv); 
+   MPI_Comm_size(MPI_COMM_WORLD,&numprocs); 
+   MPI_Comm_rank(MPI_COMM_WORLD,&myid); if (myid==0) {
+      n = atoi(argv[1]);
+      printf("\n N=%d\nAngulo: %s, %s, %s",n, argv[2], argv[3], argv[4]); 
+   }
+   MPI_Bcast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
+   // serie_seno
+   double seno, h, mySeno;
+   h = 1.0 / (double) n;
+   seno = 0.0;
+   double a = ang * h;
+   for(i = myid+1; i <= n; i += numprocs){
+      printf("\nproc %i a: %f\n", myid, ang);
+      double r;
+      r = (power(-1, (i-1)) * power(a, 2*(i-1) + 1) / fatorial(2*(i-1) + 1));
+      printf("\nproc %i sen: %f\n", myid, r);
+      seno += r;
+   }
+   printf("\nproc %i computes: %.10f\n", myid, seno);
+   mySeno = seno;
+   // mySeno = serie_seno(ang, n, h);
+   // printf("\nmySeno: %f", mySeno);
+   MPI_Reduce(&mySeno, &SENO, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+   // MPI_Bcast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
+   // // serie_coseno
+   // double coseno,  myCosn;
+   // coseno = 0.0;
+   // for(i = myid+1; i <= n;i+=numprocs){
+   //    if (i == 0) {
+   //       coseno += 1;
+   //       continue;
+   //    };
+   //    coseno += power(-1, i) * power(ang, 2*i) / fatorial(2*i);
+   // }
+   // myCosn = h*coseno;
+   // MPI_Reduce(&myCosn, &COSN, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+   // MPI_Bcast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
+   // double myTg;
+   // myTg = mySeno/myCosn;
+   // MPI_Reduce(&myTg, &TG, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+   if (myid == 0) {
+      tempoF=MPI_Wtime();
+      printf("\n Tempo Decorrido:\n%.10f\n",fabsf(tempoI-tempoF));
+      printf("\nseno:\n%.20f", SENO);
+      // printf("\nseno:\n%.20f\ncosn:\n%.20f\ntg:\n%.20f\n", SENO/n, COSN/n, TG/n);
+   }
    MPI_Finalize();
-   return (resultado);
-}
-
-double serie_coseno(double x, int termos){
-   double pi = 3.14159265358979323846;
-   int i;
-   double resultado;
-   resultado = 0;
-   x = (x*pi)/180.0;
-   for(i = 0; i < termos;i++){
-      if (i == 0) {
-         resultado += 1;
-         continue;
-      };
-      resultado += power(-1, i) * power(x, 2*i) / fatorial(2*i);
-   }
-   return (resultado);
-}
-
-double coseno(){
-   double x;
-   int termos;
-   system("clear");
-   printf("****sen(x) pelo polinomio de Taylor****\n\n");
-   printf("Digite o numero de termos da sequencia: " );
-   scanf("%d", &termos);
-   printf("\nDigite o valor de x para o calculo do cosen(x), x = ");
-   scanf("%lf", &x);
-   printf("\n\nO resultado e: %1.15lf\n\n", serie_coseno(x, termos));
-   getchar();
-   return 0;
-}
-
-double seno(){
-   double x = 45;
-   int termos= 2;
-   system("clear");
-   // printf("****sen(x) pelo polinomio de Taylor****\n\n");
-   // printf("Digite o numero de termos da sequencia: " );
-   //scanf("%d", &termos);
-   //printf("\nDigite o valor de x para o calculo do sen(x), x = ");
-   //scanf("%lf", &x);
-   printf("\n\nO resultado e: %1.15lf\n\n", serie_seno(x, termos));
-   getchar();
    return 0;
 }
 
 
-double tg(){
-   double x;
-   int termos;
-   system("clear");
-   printf("****sen(x) pelo polinomio de Taylor****\n\n");
-   printf("Digite o numero de termos da sequencia: " );
-   scanf("%d", &termos);
-   printf("\nDigite o valor de x para o calculo do sen(x), x = ");
-   scanf("%lf", &x);
-   double sen = serie_seno(x, termos);
-   double cosen = serie_coseno(x, termos);
-   printf("\n\nO resultado sen e: %1.15lf\n\n", sen);
-   printf("\n\nO resultado cosen e: %1.15lf\n\n", cosen);
-   printf("\n\nO resultado tg e: %1.15lf\n\n", sen/cosen);
-   getchar();
-   return 0;
-}
 
-int main(){
-   // tg();
-   seno();
-   // coseno();
-   return 0;
-}
--- prog_paralela/programas/pi2.c
+++ prog_paralela/programas/pi2.c
@@ -3,7 +3,6 @@
 #include <math.h>
 
 int main( int argc, char *argv[] ) {
-    // int n;
     int n, myid, numprocs, i;
     double PI25DT = 3.141592653589793238462643; 
     double pi;
